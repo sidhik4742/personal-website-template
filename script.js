6a34b40a3b2b6a688b5cfdafd6c71a994e2f7dde
sidhik4742 <sidhik64742@gmail.com>
2020-09-23 12:08:29 +0530

    bug fixing in texarea

--- script.js
+++ script.js
@@ -14,16 +14,34 @@ document.getElementById("submitBtn").addEventListener("click", (event) => {
     `name = ${name} email = ${email} phone = ${phone} message = ${message}`
   );
   if (validate()) {
-    document.getElementById("Name").style.borderColor = "#ccc";
-    document.getElementById("Email").style.borderColor = "#ccc";
-    document.getElementById("Phone").style.borderColor = "#ccc";
-    alert("Thank you for submitting form")
+    alert("Thank you for submitting form");
     document.getElementById("formSubmit").submit();
   }
 });
 
 const validate = () => {
+  let nameStatus = true;
+  let emailStatus = true;
+  let phoneStatus = true;
+  let messageStatus = true;
   console.log("This is validate function");
+  if (nameStatus) {
+    document.getElementById("Name").style.borderColor = "#ccc";
+    nameStatus = false;
+  }
+  if (emailStatus) {
+    document.getElementById("Email").style.borderColor = "#ccc";
+    emailStatus = false;
+  }
+  if (phoneStatus) {
+    document.getElementById("Phone").style.borderColor = "#ccc";
+    phoneStatus = false;
+  }
+  if (messageStatus) {
+    document.getElementById("Message").style.borderColor = "#ccc";
+    messageStatus = false;
+  }
+
   let mailFormat = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   let phoneFormat = /^\d{10}$/;
   if (name === "Name" || name.length < 4) {
@@ -38,7 +56,15 @@ const validate = () => {
     document.getElementById("Phone").style.borderColor = "red";
     console.error("Enter valid phone number");
     return false;
+  } else if (message === "Message content should be at least 10 characters...") {
+    console.error("Enter at minimum 5 character!...");
+    document.getElementById("Message").style.borderColor = "red";
+    return false;
   } else {
+    document.getElementById("Name").style.borderColor = "#ccc";
+    document.getElementById("Email").style.borderColor = "#ccc";
+    document.getElementById("Phone").style.borderColor = "#ccc";
+    document.getElementById("Message").style.borderColor = "#ccc";
     return true;
   }
 };
